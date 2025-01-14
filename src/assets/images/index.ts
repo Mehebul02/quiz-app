af7793b0d0e5468b317d6a5800c2051313273df4
Mehebul02 <mehebul2123@gmail.com>
2025-01-15 00:46:00 +0600

    Logo added

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+import logo from '../images/logo/quizLogo.png'
+
+
+export {logo}
--- src/home/Question.tsx
+++ src/home/Question.tsx
@@ -1,4 +1,5 @@
 
+import { logo } from "@/assets/images";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { useAppSelector } from "@/redux/hooks";
 
@@ -11,9 +12,10 @@ const Question = () => {
         <div>
             <div>
                 <Card className="w-[650px] ">
+                    <img className="w-32 mx-auto" src={logo} alt="logo" />
                     <CardHeader>
                         <CardTitle className="font-serif leading-relaxed">{currentQuestion.question}</CardTitle>
-                        <CardDescription>Question :{currentQuestionIndex + 1}</CardDescription>
+                        <CardDescription>Question :{currentQuestionIndex + 1} of {question.length}</CardDescription>
                     </CardHeader>
                     <CardContent>
 
